957320a0e399341e19ad12f8f7ef33cabfb43aa1
mikeyhagen <mikeyhagen@gmail.com>
2022-10-15 20:28:21 -0500

    Initial set up

---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer();
+
+server.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
+});
